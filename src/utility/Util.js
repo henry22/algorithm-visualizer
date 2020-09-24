d3576d96d494f630ea03e4c04983dc5b48579224
henry22 <henry811010@gmail.com>
2020-09-24 17:19:35 +0800

    generate random number without duplicate

--- src/utility/Util.js
+++ src/utility/Util.js
@@ -1,15 +1,18 @@
 import {uid} from 'react-uid'
 
 export function generateRandomArray(length) {
-  let randomItems = []
+  const set = new Set()
+  const randomItems = []
 
   for(let i = 0; i < length; i++) {
     const randomNumber = Math.floor(Math.random() * 101)
 
-    if (randomItems.indexOf(randomNumber) === -1) {
+    if (!set.has(randomNumber)) {
       randomItems.push({ id: uid(Math.random()), itemValue: randomNumber, color: '#2d8ae2' })
+      set.add(randomNumber)
     }
   }
+
   return randomItems
 }
 
