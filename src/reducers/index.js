903944420f34dac02b3fab8eacf6dbd211bff60c
henry22 <henry811010@gmail.com>
2020-09-29 10:54:04 +0800

    create current sorted reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux'
 import { array } from './array'
 import { algorithm } from './algorithm'
-import { currentBubbleSortTwo } from './bubbleSort'
 import { currentSwapper } from './swapper'
+import { currentBubbleSortTwo } from './bubbleSort'
+import { currentSorted } from './sorted'
 
 const rootReducer = combineReducers({
   array,
   algorithm,
+  currentSwapper,
   currentBubbleSortTwo,
-  currentSwapper
+  currentSorted
 })
 
 export default rootReducer
