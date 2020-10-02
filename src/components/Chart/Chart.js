9ca260ee6337e47f1760461a06bb63e4c9aee5af
henry22 <henry811010@gmail.com>
2020-10-02 20:19:29 +0800

    change chart height to auto

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -22,7 +22,7 @@ const Chart = (props) => {
   let liWidth = 32;
 
   return (
-    <div style={{ display: 'inline-flex', justifyItems: 'center', alignItems: 'flex-end', height: '680px', overflow: 'hidden' }}>
+    <div style={{ display: 'inline-flex', justifyItems: 'center', alignItems: 'flex-end', height: 'auto', overflow: 'hidden' }}>
       {array.length ? array.map((number, index) => {
         let backgroundColor;
         if (currentSwapper.includes(index)) {
