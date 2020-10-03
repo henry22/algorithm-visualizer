970ecaca04942b6ef28505c244a9dc07ff7ed350
henry22 <henry811010@gmail.com>
2020-10-03 18:23:00 +0800

    remove unused files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Visualizer from './components/Visualizer';
 import Main from './components/Main'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Visualizer /> */}
       <Main />
     </div>
   )
